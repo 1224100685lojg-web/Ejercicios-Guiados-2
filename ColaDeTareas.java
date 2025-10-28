7d5f717fa9f0c9a926aa1339b7ec816a26b5699a
1224100685lojg-web <1224100685.lojg@gmail.com>
2025-10-28 09:14:07 -0600

    Update ColaDeTareas.java

--- ColaDeTareas.java
+++ ColaDeTareas.java
@@ -7,16 +7,24 @@ import java.util.LinkedList;
 import java.util.Queue;
 /**
  *
- * @author Owen_04
+* Clase ColaDeTareas
+ * -------------------
+ * Implementa una cola de tareas utilizando la interfaz Queue de Java (FIFO).
+ * Cada tarea se almacena como una cadena de texto y se gestiona con métodos 
+ * para cargar, visualizar, ejecutar y contar las tareas pendientes.
+ 
+ * @author Luis Owen Jaramillo Guerrero
  */
 
+// Estructura principal de la cola
 public class ColaDeTareas {
     private Queue<String> cola;
 
+//Constructor: inicializa la cola vacía usando LinkedList.
     public ColaDeTareas() {
         cola = new LinkedList<>();
     }
-
+//     * Carga un conjunto inicial de tareas predeterminadas. Limpia la cola antes de agregar nuevas tareas.
     public void cargarTareas() {
         cola.clear();
         cola.add("Preparar café");
@@ -24,22 +32,24 @@ public class ColaDeTareas {
         cola.add("Iniciar proyecto");
     }
 
+    //Devuelve la tarea ubicada al frente de la cola sin eliminarla.
     public String verFrente() {
         return cola.peek();
     }
-
+//Elimina y devuelve la tarea ubicada al frente de la cola.
     public String ejecutarTarea() {
         return cola.poll();
     }
-
+// Obtiene el número total de tareas pendientes en la cola.
     public int obtenerTamaño() {
         return cola.size();
     }
-
+//Devuelve todas las tareas actualmente en la cola.
     public Queue<String> verTodas() {
         return cola;
     }
 }
+
 
 
 
